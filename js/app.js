b0d47048c04d94d87584b2511c74aa007a53531c
Artem <skealyxa@gmail.com>
2021-10-21 01:21:12 +0700

    Сделал анимации

--- js/app.js
+++ js/app.js
@@ -1,9 +1,58 @@
-const bottonMenu = document.querySelector('.header__menu-button');
-if(bottonMenu){
-    const menuBody = document.querySelector('.menu__body');
-    bottonMenu.addEventListener("click", function(e) {
+const buttonMenu = document.querySelector('.header__menu-button');
+const menuBody = document.querySelector('.menu');
+if(buttonMenu){
+    buttonMenu.addEventListener("click", function(e) {
+        arrow.classList.toggle('_active');
         document.body.classList.toggle('_lock');
-        bottonMenu.classList.toggle('_active');
+        buttonMenu.classList.toggle('_active');
         menuBody.classList.toggle('_active');
     })
 }
+const header = document.querySelector('.header');
+window.addEventListener('scroll', function() {
+    if(header){
+        let scrolled = window.pageYOffset;
+        if( scrolled >= 10 ) {
+            header.classList.add('_active');
+        }   
+        if( scrolled < 10 ) {     
+            header.classList.remove('_active');
+        }
+    };
+})
+
+
+const title_list1 = document.getElementById("title_list1");
+const arrow = document.querySelector('.welcome-section__arrow')
+function handleButtonClick() {
+    title_list1.scrollIntoView({block: "center", behavior: "smooth"});
+ }
+
+ arrow.addEventListener('click', handleButtonClick);
+
+ const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+ if(menuLinks.length>0){
+     menuLinks.forEach(menuLink=>{
+         menuLink.addEventListener("click", onMenuLinkClick);
+     });
+     function onMenuLinkClick(e) {
+         const menuLink =e.target;
+         if(menuLink.dataset.goto && document. querySelector(menuLink.dataset.goto)){
+             const gotoBlock = document. querySelector(menuLink.dataset.goto);
+             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - 200;
+             
+             if(buttonMenu.classList.contains('_active')){
+                document.body.classList.remove('_lock');
+                buttonMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+                arrow.classList.remove('_active');
+             }
+
+             window.scrollTo({
+                 top:gotoBlockValue,
+                 behavior: "smooth"
+             });
+             e.preventDefault();
+         }
+     }
+ }
