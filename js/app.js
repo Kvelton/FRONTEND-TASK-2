7146725c160ea00e28e872b329d9514cb4d5a1dd
Buldaev Aldar <buldaev.aldar@gmail.com>
2021-11-24 13:05:03 +0800

    Допилен скрипт и SVG спрайт, форматирование

--- js/app.js
+++ js/app.js
@@ -1,59 +1,63 @@
-const buttonMenu = document.querySelector('.header__menu-button');
-const menuBody = document.querySelector('.menu');
-if (buttonMenu) {
-    buttonMenu.addEventListener("click", function (e) {
-        arrow.classList.toggle('_active');
-        document.body.classList.toggle('_lock');
-        buttonMenu.classList.toggle('_active');
-        menuBody.classList.toggle('_active');
-    })
-}
+function init() {
+    const buttonMenu = document.querySelector('.header__menu-button');
+    const menuBody = document.querySelector('.menu');
+    if (buttonMenu) {
+        buttonMenu.addEventListener("click", function (e) {
+            arrow.classList.toggle('_active');
+            document.body.classList.toggle('_lock');
+            buttonMenu.classList.toggle('_active');
+            menuBody.classList.toggle('_active');
+        })
+    }
 
-window.addEventListener('scroll', handleWindowScroll ); 
-function handleWindowScroll(){
-    const header = document.querySelector('.header');
-    if (header) {
-        let scrolled = window.pageYOffset;
-        if (scrolled >= 10) {
-            header.classList.add('_active');
-        }
-        if (scrolled < 10) {
-            header.classList.remove('_active');
-        }
-    };
-}
-
-const title_list1 = document.querySelector('.sightseens');
-const arrow = document.querySelector('.welcome-section__arrow');
-function handleButtonClick() {
-    title_list1.scrollIntoView({ block: "start", behavior: "smooth" });
-}
-
-arrow.addEventListener("click", handleButtonClick);
-
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-if (menuLinks.length > 0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener("click", onMenuLinkClick);
-    });
-    function onMenuLinkClick(e) {
-        const menuLink = e.target;
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - 200;
-
-            if (buttonMenu.classList.contains('_active')) {
-                document.body.classList.remove('_lock');
-                buttonMenu.classList.remove('_active');
-                menuBody.classList.remove('_active');
-                arrow.classList.remove('_active');
+    window.addEventListener('scroll', handleWindowScroll);
+    function handleWindowScroll() {
+        const header = document.querySelector('.header');
+        if (header) {
+            let scrolled = window.pageYOffset;
+            if (scrolled >= 10) {
+                header.classList.add('_active');
             }
+            if (scrolled < 10) {
+                header.classList.remove('_active');
+            }
+        };
+    }
 
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: "smooth"
-            });
-            e.preventDefault();
+    const title_list1 = document.querySelector('.sightseens');
+    const arrow = document.querySelector('.welcome-section__arrow');
+    function handleButtonClick() {
+        title_list1.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+
+    arrow.addEventListener("click", handleButtonClick);
+
+    const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+    if (menuLinks.length > 0) {
+        menuLinks.forEach(menuLink => {
+            menuLink.addEventListener("click", onMenuLinkClick);
+        });
+        function onMenuLinkClick(e) {
+            const menuLink = e.target;
+            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+                const gotoBlock = document.querySelector(menuLink.dataset.goto);
+                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - 200;
+
+                if (buttonMenu.classList.contains('_active')) {
+                    document.body.classList.remove('_lock');
+                    buttonMenu.classList.remove('_active');
+                    menuBody.classList.remove('_active');
+                    arrow.classList.remove('_active');
+                }
+
+                window.scrollTo({
+                    top: gotoBlockValue,
+                    behavior: "smooth"
+                });
+                e.preventDefault();
+            }
         }
     }
 }
+
+window.addEventListener("load", init);
