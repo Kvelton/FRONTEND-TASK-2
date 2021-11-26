df6372601f7da1218156ac00b8d876a196879cc8
artemkrutoy <skealyxa@gmail.com>
2021-11-26 16:58:29 +0700

    Исправил ошибки с js кодом

--- js/app.js
+++ js/app.js
@@ -1,63 +1,75 @@
+let buttonMenu;
+let menuBody;
+let title_list1;
+let arrow;
+let menuLinks;
+
 function init() {
-    const buttonMenu = document.querySelector('.header__menu-button');
-    const menuBody = document.querySelector('.menu');
+    buttonMenu = document.querySelector('.header__menu-button');
+    menuBody = document.querySelector('.menu');
     if (buttonMenu) {
-        buttonMenu.addEventListener("click", function (e) {
-            arrow.classList.toggle('_active');
-            document.body.classList.toggle('_lock');
-            buttonMenu.classList.toggle('_active');
-            menuBody.classList.toggle('_active');
-        })
+        buttonMenu.addEventListener("click", onMenuClick)
     }
 
     window.addEventListener('scroll', handleWindowScroll);
-    function handleWindowScroll() {
-        const header = document.querySelector('.header');
-        if (header) {
-            let scrolled = window.pageYOffset;
-            if (scrolled >= 10) {
-                header.classList.add('_active');
-            }
-            if (scrolled < 10) {
-                header.classList.remove('_active');
-            }
-        };
-    }
 
-    const title_list1 = document.querySelector('.sightseens');
-    const arrow = document.querySelector('.welcome-section__arrow');
-    function handleButtonClick() {
-        title_list1.scrollIntoView({ block: "start", behavior: "smooth" });
-    }
+    title_list1 = document.querySelector('.sightseens');
+    arrow = document.querySelector('.welcome-section__arrow');
 
     arrow.addEventListener("click", handleButtonClick);
 
-    const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+    menuLinks = document.querySelectorAll('.menu__link[data-goto]');
     if (menuLinks.length > 0) {
         menuLinks.forEach(menuLink => {
             menuLink.addEventListener("click", onMenuLinkClick);
         });
-        function onMenuLinkClick(e) {
-            const menuLink = e.target;
-            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-                const gotoBlock = document.querySelector(menuLink.dataset.goto);
-                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - 200;
+    }
 
-                if (buttonMenu.classList.contains('_active')) {
-                    document.body.classList.remove('_lock');
-                    buttonMenu.classList.remove('_active');
-                    menuBody.classList.remove('_active');
-                    arrow.classList.remove('_active');
-                }
+}
 
-                window.scrollTo({
-                    top: gotoBlockValue,
-                    behavior: "smooth"
-                });
-                e.preventDefault();
-            }
+window.addEventListener("DOMContentLoaded", init);
+
+function onMenuClick(e) {
+    arrow.classList.toggle('_active');
+    document.body.classList.toggle('_lock');
+    buttonMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+}
+
+function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+        const gotoBlock = document.querySelector(menuLink.dataset.goto);
+        const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - 200;
+
+        if (buttonMenu.classList.contains('_active')) {
+            document.body.classList.remove('_lock');
+            buttonMenu.classList.remove('_active');
+            menuBody.classList.remove('_active');
+            arrow.classList.remove('_active');
         }
+
+        window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth"
+        });
+        e.preventDefault();
     }
 }
 
-window.addEventListener("load", init);
+function handleButtonClick() {
+    title_list1.scrollIntoView({ block: "start", behavior: "smooth" });
+}
+
+function handleWindowScroll() {
+    const header = document.querySelector('.header');
+    if (header) {
+        let scrolled = window.pageYOffset;
+        if (scrolled >= 10) {
+            header.classList.add('_active');
+        }
+        if (scrolled < 10) {
+            header.classList.remove('_active');
+        }
+    };
+}
