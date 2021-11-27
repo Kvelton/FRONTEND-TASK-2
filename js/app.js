58d8b57044c13d577fcd3d20fa11fe75f773efd8
artemkrutoy <skealyxa@gmail.com>
2021-11-27 13:52:36 +0700

    Добавил кнопку больше туров

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ let menuBody;
 let title_list1;
 let arrow;
 let menuLinks;
+let buttonToggleItems
+let listItemsContainer
 
 function init() {
     buttonMenu = document.querySelector('.header__menu-button');
@@ -24,10 +26,35 @@ function init() {
             menuLink.addEventListener("click", onMenuLinkClick);
         });
     }
+    buttonToggleItems = document.getElementsByClassName('tours-subtext')
+    if(buttonToggleItems.length === 0) {
+      throw new Error('Кнопка не найдена в document');
+    }
+    
+    listItemsContainer
+    listItemsContainer = document.getElementsByClassName('tours')
+    
+    buttonToggleItems[0].addEventListener(
+      'click',
+      (event) =>
+        handleToggleItemsButtonClick(listItemsContainer[0], event)
+    )
 
 }
 
 window.addEventListener("DOMContentLoaded", init);
+
+function handleToggleItemsButtonClick(listItemsContainer, pointerEvent) {
+    const classForVisibleHiddenItems = 'tours-block--hidden-visible';
+    
+    if(pointerEvent.currentTarget.innerText === 'Больше туров') {
+      pointerEvent.currentTarget.innerText = 'Меньше туров'
+      listItemsContainer.classList.add(classForVisibleHiddenItems)
+    } else {
+        pointerEvent.currentTarget.innerText = 'Больше туров'
+        listItemsContainer.classList.remove(classForVisibleHiddenItems)
+    }
+  }
 
 function onMenuClick(e) {
     arrow.classList.toggle('_active');
